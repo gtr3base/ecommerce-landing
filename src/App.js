6576bc645aebeaf7c030679168f8f633c228eb4c
gtr3base <aloxaryabov@gmail.com>
2025-05-20 22:38:55 +0300

    Fix deployment config

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     { id: 1, name: 'Wireless Headphones', price: 99.99, image: 'https://jblstore.com.ph/cdn/shop/files/Tune720BT_Blue_1.png?v=1747046882' },
     { id: 2, name: 'Smart Watch', price: 199.99, image: 'https://www.istudiosg.com/cdn/shop/files/Apple_Watch_Series_10_42mm_GPS_Silver_Aluminum_Sport_Band_Denim_PDP_Image_Position_1__GBEN.jpg?v=1727185024' },
     { id: 3, name: 'Bluetooth Speaker', price: 59.99, image: 'https://www.jbhifi.com.au/cdn/shop/products/597794-Product-0-I-637945070583397301.jpg?v=1658874344' },
+    { id: 4, name: 'Laptop', price: 899, image: 'https://my-store.msi.com/cdn/shop/files/M15B13M_1_869x869.png?v=1712221181'},
   ];
 
   const addToCart = (product) => {
